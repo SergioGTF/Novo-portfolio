ced85f48d2f9a48c3ebba06ff2c822b718f1fbe0
SergioGTF <sergio23300033@aluno.cesupa.br>
2025-03-16 14:12:32 -0300

    Adição do background e estilização da página projetos, contato e adição de função no javascript que faz com que a página sobre sempre seja carregada

--- script.js
+++ script.js
@@ -2,15 +2,25 @@ document.addEventListener('DOMContentLoaded', function() {
     const links = document.querySelectorAll('nav ul li a');
     const conteudo = document.getElementById('conteudo');
 
+    function carregarPagina(page) {
+        fetch(page)
+            .then(response => response.text())
+            .then(data => {
+                conteudo.innerHTML = data;
+            })
+            .catch(error => {
+                console.error('Erro ao carregar a página:', error);
+                conteudo.innerHTML = '<p>Desculpe, não foi possível carregar a página.</p>';
+            });
+    }
+
+    carregarPagina('Sobre.html');
+
     links.forEach(link => {
         link.addEventListener('click', function(event) {
             event.preventDefault();
             const page = this.getAttribute('data-page');
-            fetch(page)
-                .then(response => response.text())
-                .then(data => {
-                    conteudo.innerHTML = data;
-                });
+            carregarPagina(page);
         });
     });
 });
